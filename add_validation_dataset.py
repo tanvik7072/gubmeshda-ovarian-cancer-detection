d1fc288e35ac491b8b955fecb30b5b6a44800d88
Tanvi Kapil <66367698+tanvik7072@users.noreply.github.com>
2024-03-25 17:03:45 +0000

    Output directory and training images directory paths updated

--- add_validation_dataset.py
+++ add_validation_dataset.py
@@ -1,11 +1,11 @@
 # Set the paths
-valid_images_path = '/kaggle/output/valid_images'
+valid_images_path = '/kaggle/working/valid_images'
 
 # Create the validation folder if it doesn't exist
 os.makedirs(valid_images_path, exist_ok=True)
 
 # Get the list of all images in the train folder
-all_images = os.listdir(train_images_path)
+all_images = os.listdir(folder_path)
 
 # Calculate the number of images to move (25%)
 num_images_to_move = int(0.25 * len(all_images))
@@ -15,6 +15,6 @@ images_to_move = random.sample(all_images, num_images_to_move)
 
 # Move the selected images to the validation folder
 for image in images_to_move:
-    src_path = os.path.join(train_images_path, image)
+    src_path = os.path.join(folder_path, image)
     dest_path = os.path.join(valid_images_path, image)
     shutil.move(src_path, dest_path)
