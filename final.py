fa9797757301fe702fc121992e9521b25e7b80e6
Mitchell-James <2619137g@student.gla.ac.uk>
2024-03-27 13:20:38 +0000

    Shutil Imported

--- final.py
+++ final.py
@@ -6,7 +6,7 @@
 
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import random
+import random, shutil
 from PIL import Image, ImageEnhance
 from torch.utils.data import DataLoader
 
