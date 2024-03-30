ca9fe7ad99567a1f871f51b0dbbde4d857a706dd
Tanvi Kapil <66367698+tanvik7072@users.noreply.github.com>
2024-03-30 15:19:37 +0000

    Import statement for optim added

--- final.py
+++ final.py
@@ -17,6 +17,7 @@ import os
 
 import tensorflow as tf
 import cv2
+from torch import optim
 
 # You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All"
 # You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
