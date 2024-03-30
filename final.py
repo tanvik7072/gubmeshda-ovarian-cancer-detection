be517ac05da71cf913311f70da4a2ab379448acf
Tanvi Kapil <66367698+tanvik7072@users.noreply.github.com>
2024-03-30 14:43:24 +0000

    Import statement for models added

--- final.py
+++ final.py
@@ -9,6 +9,7 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import random, shutil
 from PIL import Image, ImageEnhance
 from torch.utils.data import DataLoader
+from torchvision import models
 
 # Input data files are available in the read-only "../input/" directory
 # For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
